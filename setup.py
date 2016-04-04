785a139ba74fa5f47f88f841d5af046a6c266542
Necdet Can Ateşman <can@strg.at>
2016-04-04 16:55:52 +0200

    Release: 0.3.5

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.session',
-    version='0.3.4',
+    version='0.3.5',
     description='Session management for The SCORE Framework',
     long_description=README,
     author='strg.at',
